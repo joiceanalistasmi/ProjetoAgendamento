1e53c8494b3ef76580b4d14e5f592b5f3a5699b1
Joice <joice.smipm@gmail.com>
2025-07-17 16:20:00 -0300

    v1 17/07 16:19

--- wp-content/themes/prjDaily/buscarHorarios.php
+++ wp-content/themes/prjDaily/buscarHorarios.php
@@ -3,7 +3,7 @@ include("conexao.php");
 
 //buscar os dados do banco de dados
 if (isset($_POST['buscar'])) {      
-    $nome = $_POST['nome'];
+    $nome_servidor = $_POST['nome_servidor'];
     $email = $_POST['email'];
     $tipo = $_POST['tipo'];
     $data_agendamento = $_POST['data_agendamento'];
@@ -12,16 +12,16 @@ if (isset($_POST['buscar'])) {
     $status = $_POST['status'];
 
     // Verifica se os campos estão preenchidos
-    if (empty($nome) || empty($email) || empty($tipo) || empty($data_agendamento) || empty($horario) || empty($turno) || empty($status)) {
+    if (empty($nome_servidor) || empty($email) || empty($tipo) || empty($data_agendamento) || empty($horario) || empty($turno) || empty($status)) {
         echo "<script>alert('Por favor, preencha todos os campos.');</script>";
     } else {
         // Consulta ao banco de dados
-        $sqlBuscarHorarios = mysqli_query($conexao, "SELECT * FROM agendamentos WHERE nome LIKE '%$nome%' AND email LIKE '%$email%' AND tipo LIKE '%$tipo%' AND data_agendamento='$data_agendamento' AND horario='$horario' AND turno='$turno' AND status='$status'")
+        $sqlBuscarHorarios = mysqli_query($conexao, "SELECT * FROM agendamentos WHERE nome_servidor LIKE '%$nome_servidor%' AND email LIKE '%$email%' AND tipo LIKE '%$tipo%' AND data_agendamento='$data_agendamento' AND horario='$horario' AND turno='$turno' AND status='$status'")
             or die("Erro ao buscar os registros. " . mysqli_error($conexao));
 
         // Exibir resultados
         while ($row = mysqli_fetch_assoc($sqlBuscarHorarios)) {
-            echo "Nome: " . $row['nome'] . "<br>";
+            echo "nome_servidor: " . $row['nome_servidor'] . "<br>";
             echo "Email: " . $row['email'] . "<br>";
             echo "Tipo: " . $row['tipo'] . "<br>";
             echo "Data do Agendamento: " . $row['data_agendamento'] . "<br>";
--- wp-content/themes/prjDaily/processar_agendamento.php
+++ wp-content/themes/prjDaily/processar_agendamento.php
@@ -1,23 +1,24 @@
 <?php
 
 include("conexao.php");
-
+session_start();
 if (
-    isset($_POST['nome_servidor']) && isset($_POST['email']) && isset($_POST['tipo']) &&
+    isset($_POST['nome_servidor']) && isset($_POST['email']) && isset($_POST['tipo']) && isset($_POST['tipo_de_usuario']) && 
     isset($_POST['data_agendamento']) && isset($_POST['horario']) &&
     isset($_POST['turno']) && isset($_POST['status'])
 ) {
 
     $nome_servidor = $_POST["nome_servidor"];
+    $tipo = $_POST["tipo_de_usuario"];
     $email = $_POST["email"];
     $tipo = $_POST["tipo"];
     $data_agendamento = $_POST["data_agendamento"];
     $horario = $_POST["horario"];
     $turno = $_POST["turno"];
     $status = $_POST["status"];
-
+  
     if (
-        empty($nome_servidor) || empty($email) || empty($tipo) ||
+        empty($nome_servidor) || empty($tipo_de_usuario) || empty($email) || empty($tipo) ||
         empty($data_agendamento) || empty($horario) ||
         empty($turno) || empty($status)
     ) {
@@ -25,16 +26,17 @@ if (
         exit;
     } else {
         $sqlGravarAgenda = mysqli_query($conexao,
-            "INSERT INTO agendamentos (nome_servidor, email, tipo, data_agendamento, horario, turno, status)
-            VALUES ('$nome_servidor','$email','$tipo','$data_agendamento', '$horario', '$turno', '$status')"
+            "INSERT INTO agendamentos (nome_servidor, tipo_de_usuario, email, tipo, data_agendamento, horario, turno, status)
+            VALUES ('$nome_servidor','$tipo_de_usuario','$email','$tipo','$data_agendamento', '$horario', '$turno', '$status')"
         ) or die("Erro ao gravar o registro. " . mysqli_error($conexao));
-
-        header('Location: lista.php');
+            echo "<script>alert('Registro gravado com sucesso!');</script>";
+            echo "<script>window.location.href = 'lista.php';</script>";
         exit;
     }
 }else {
     echo "<script>alert('Dados não recebidos corretamente.'); window.history.back();</script>";
     exit;
+    session_destroy();
 }
 
 ?>
