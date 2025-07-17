b3380f59855c605d5d5465153fe3da29f784cd5b
Joice <joice.smipm@gmail.com>
2025-07-17 10:25:54 -0300

    v1 17/07 10:16

--- wp-content/themes/prjDaily/processar_agendamento.php
+++ wp-content/themes/prjDaily/processar_agendamento.php
@@ -1,13 +1,13 @@
 <?php
 include("conexao.php");
 
-    $nome = $_POST("nome");
-    $email = $_POST("email");
-    $tipo = $_POST("tipo");
-    $data_agendamento = $_POST("data_agendamento");
-    $horario = $_POST("horario");
-    $turno = $_POST("turno");
-    $status = $_POST("status");
+$nome = $_POST("nome");
+$email = $_POST("email");
+$tipo = $_POST("tipo");
+$data_agendamento = $_POST("data_agendamento");
+$horario = $_POST("horario");
+$turno = $_POST("turno");
+$status = $_POST("status");
 
     if (empty($nome) && empty($email) && empty("tipo") && empty("$data_agendamento")  && empty("$horario") &&
         empty("$turno") && empty("$status")){
