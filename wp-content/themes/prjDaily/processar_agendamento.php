e49c29d3e3b4e700127e0b008c83fa256dd49033
Joice <joice.smipm@gmail.com>
2025-07-17 11:18:54 -0300

    v1 17/07 11:18

--- wp-content/themes/prjDaily/processar_agendamento.php
+++ wp-content/themes/prjDaily/processar_agendamento.php
@@ -1,17 +1,28 @@
 <?php
+
 include("conexao.php");
 
-$nome = $_POST("nome");
-$email = $_POST("email");
-$tipo = $_POST("tipo");
-$data_agendamento = $_POST("data_agendamento");
-$horario = $_POST("horario");
-$turno = $_POST("turno");
-$status = $_POST("status");
+ if ( isset($_POST['nome'])  && isset($_POST['email'])  && isset($_POST['tipo'])  && isset($_POST['data_agendamento'])  &&  isset($_POST['horario'])  &&   
+        isset($_POST['turno'])  && isset($_POST['status']))  {
+   
+    $nome = $_POST("nome");
+    $email = $_POST("email");
+    $tipo = $_POST("tipo");
+    $data_agendamento = $_POST("data_agendamento");
+    $horario = $_POST("horario");
+    $turno = $_POST("turno");
+    $status = $_POST("status");
 
     if (empty($nome) && empty($email) && empty("tipo") && empty("$data_agendamento")  && empty("$horario") &&
         empty("$turno") && empty("$status")){
-            echo " <script> alert('por favor, preencha todos os campos.');</script>";
+            echo " <script> alert('Por favor, preencha todos os campos.');</script>";
     }
+    
+    //dados do banco de dados para gravar e fazer a verificação dos dados  
+    $sqlGravarAgenda = mysqli_query($conexao,"insert into agendamentos(nome, email, tipo, data_agendamento, horario, turno, status)) 
+        values ('$nome','$email','$tipo','$data_agendamento', '$horario', '$turno', '$status')") or die("Erro ao gravar o registro. " . mysqli_error($conexao));
+    header('Location: lista.php'); // 
+        }
+
 
 ?>  
